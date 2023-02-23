595b5da9e94e84b26079255b077359303970b27a
Marc-Antoine <magiguere@mirego.com>
2023-02-23 09:03:17 -0500

    fix rust-app

--- rust-app/tests/lib_test.rs
+++ rust-app/tests/lib_test.rs
@@ -2,5 +2,5 @@ use rust_app::app;
 
 #[test]
 fn test_app() {
-    assert_eq!(app(), "Man, I do love cats, BUT IN RUUUUUUUST BABYYYYYYYYY");
+    assert_eq!(app(), "Man, I really love cats, BUT IN RUUUUUUUST BABYYYYYYYYY");
 }
