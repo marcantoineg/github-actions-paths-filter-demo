b7bfb51cb4ea63f7b761d5c449d29452ab6e047b
Marc-Antoine <magiguere@mirego.com>
2023-02-23 09:03:01 -0500

    fix go-app

--- go-app/app_test.go
+++ go-app/app_test.go
@@ -3,7 +3,7 @@ package app
 import "testing"
 
 func TestApp(t *testing.T) {
-	if app() != "Man, I do love cats, in golang." {
+	if app() != "Man, I really love cats, in golang." {
 		t.Error("app() did not return the expected string")
 	}
 }
